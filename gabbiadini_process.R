e942bd1a6a1b51d59e13242d5aea94e0ae433d3d
Joe Hilgard <jhilgard@gmail.com>
2016-04-19 09:10:23 -0400

    Comparing process model results across moderators.

--- gabbiadini_process.R
+++ gabbiadini_process.R
@@ -7,13 +7,13 @@ library(lavaan)
 dat = read.csv("data_set_PLOS.csv")
 
 # Modeling indirect effect using column "avatarID" as they did ----
-modelSEM.indirect = {
+modelSEM1 = {
   "emp_scal ~ b*mas_beli +
               c*cond101 + 
               cw*gender_codxcond101 + 
               cx*avatad_IDcentxcond101 +
               cwx*avatad_IDcentxgendercodxcond101
-  mas_beli ~ a*cond101 + 
+   mas_beli ~ a*cond101 + 
               aw*gender_codxcond101 + 
               ax*avatad_IDcentxcond101 +
               awx*avatad_IDcentxgendercodxcond101
@@ -28,22 +28,29 @@ modelSEM.indirect = {
 #ab := a*b + aw*b + ax*b + awx*b # WIP: I don't know what I'm doing
 #total := ab + c + cw + cx + cwx # WIP: I don't know what I'm doing
 
-model.sem.indirect = sem(modelSEM.indirect, data = dat, se = "boot")
-summary(model.sem.indirect)
+fit1 = sem(modelSEM1, data = dat, se = "boot")
+summary(fit1)
 
 
 # Modeling indirect effect using full scale composite "avatar_id" ----
 # What happens if the full scale is substituted in the analysis?
+# Pluck out the bad obs
+dat$avatar_id_wishful_id4[dat$avatar_id_wishful_id4 > 7] = NA
+dat$avatar_id_wishful_id5[dat$avatar_id_wishful_id5 > 7] = NA
+# Make mean
+dat$avatar_id_fix = dat %>% 
+  select(avatar_id_embodied_presence1:avatar_id_char_empathy4) %>% 
+  apply(., 1, mean)
 # Make all the interaction terms
 dat = dat %>% 
-  mutate(IDc = avatar_id - mean(avatar_id),
+  mutate(IDc = avatar_id_fix - mean(avatar_id_fix, na.rm = T),
          IDcxGender = IDc * gend_con,
          IDcxcond101 = IDc * cond101,
          IDcxcond101xGender = IDc * gend_con * cond101
   )
 
 # Make the model:
-modelSEM2.indirect = {
+modelSEM2 = {
   "emp_scal ~ b*mas_beli +
               c*cond101 + 
               cw*gender_codxcond101 + 
@@ -61,5 +68,58 @@ modelSEM2.indirect = {
   totalF := indirectF + c + cx # cw and cwx are zero"
 }
 
-model.sem2.indirect = sem(modelSEM2.indirect, data = dat, se = "boot")
-summary(model.sem2.indirect)
+fit2 = sem(modelSEM2, data = dat, se = "boot")
+summary(fit2)
+
+# Full scales for MRNI and empathy ----
+dat$emp_scal_full = dat %>% 
+  select(Empathy_comprensione:Empathy_affettuosita, 
+         Empathy_serenitaR:Empathy_disinteresseR) %>% 
+  apply(., 1, mean)
+
+modelSEM3 = {
+  "emp_scal_full ~ b*full_MRNI +
+                  c*cond101 + 
+                   cw*gender_codxcond101 + 
+                  cx*avatad_IDcentxcond101 +
+                   cwx*avatad_IDcentxgendercodxcond101
+
+  full_MRNI ~ a*cond101 + 
+             aw*gender_codxcond101 + 
+             ax*avatad_IDcentxcond101 +
+              awx*avatad_IDcentxgendercodxcond101
+
+  # Indirect and total effects for males (gend_cod == 1)
+  indirectM := a*b + aw*b + ax*b + awx*b
+  totalM := indirectM + c + cw + cx + cwx
+  # Indirect and total effects for females (gend_cod == 0)
+  indirectF := a*b + ax*b # aw*b and awx*b are zero
+  totalF := indirectF + c + cx # cw and cwx are zero"
+}
+
+fit3 = sem(modelSEM3, data = dat, se = "boot")
+summary(fit3)
+
+# Full scales for MRNI, empathy, and avatar-id
+modelSEM4 = {
+  "emp_scal_full ~ b*full_MRNI +
+  c*cond101 + 
+  cw*gender_codxcond101 + 
+  cx*IDcxcond101 +
+  cwx*IDcxcond101xGender
+
+  full_MRNI ~ a*cond101 + 
+  aw*gender_codxcond101 + 
+  ax*IDcxcond101 +
+  awx*IDcxcond101xGender
+
+  # Indirect and total effects for males (gend_cod == 1)
+  indirectM := a*b + aw*b + ax*b + awx*b
+  totalM := indirectM + c + cw + cx + cwx
+  # Indirect and total effects for females (gend_cod == 0)
+  indirectF := a*b + ax*b # aw*b and awx*b are zero
+  totalF := indirectF + c + cx # cw and cwx are zero"
+}
+
+fit4 = sem(modelSEM4, data = dat, se = "boot")
+summary(fit4)
