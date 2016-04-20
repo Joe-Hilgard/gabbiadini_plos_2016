5a9fcec2669cb8f79529c3585f008dc4b4dadae6
Joe Hilgard <jhilgard@gmail.com>
2016-04-19 23:41:11 -0400

    Fixed SEMs to parallel Model 11, not Model 12

--- gabbiadini_process.R
+++ gabbiadini_process.R
@@ -6,34 +6,13 @@ library(lavaan)
 
 dat = read.csv("data_set_PLOS.csv")
 
-# Modeling indirect effect using column "avatarID" as they did ----
-modelSEM1 = {
-  "emp_scal ~ b*mas_beli +
-              c*cond101 + 
-              cw*gender_codxcond101 + 
-              cx*avatad_IDcentxcond101 +
-              cwx*avatad_IDcentxgendercodxcond101
-   mas_beli ~ a*cond101 + 
-              aw*gender_codxcond101 + 
-              ax*avatad_IDcentxcond101 +
-              awx*avatad_IDcentxgendercodxcond101
-  # Indirect and total effects for males (gend_cod == 1)
-  indirectM := a*b + aw*b + ax*b + awx*b
-  totalM := indirectM + c + cw + cx + cwx
-  # Indirect and total effects for females (gend_cod == 0)
-  indirectF := a*b + ax*b # aw*b and awx*b are zero
-  totalF := indirectF + c + cx # cw and cwx are zero"
-}
-# Is it possible to retrieve unconditional indirect and total effects?
-#ab := a*b + aw*b + ax*b + awx*b # WIP: I don't know what I'm doing
-#total := ab + c + cw + cx + cwx # WIP: I don't know what I'm doing
-
-fit1 = sem(modelSEM1, data = dat, se = "boot")
-summary(fit1)
-
-
-# Modeling indirect effect using full scale composite "avatar_id" ----
-# What happens if the full scale is substituted in the analysis?
+# Make the full-composite variables
+# Full scales for MRNI and empathy 
+dat$emp_scal_full = dat %>% 
+  select(Empathy_comprensione:Empathy_affettuosita, 
+         Empathy_serenitaR:Empathy_disinteresseR) %>% 
+  apply(., 1, mean)
+# Full scales for avatar-identification
 # Pluck out the bad obs
 dat$avatar_id_wishful_id4[dat$avatar_id_wishful_id4 > 7] = NA
 dat$avatar_id_wishful_id5[dat$avatar_id_wishful_id5 > 7] = NA
@@ -49,6 +28,36 @@ dat = dat %>%
          IDcxcond101xGender = IDc * gend_con * cond101
   )
 
+# Modeling indirect effect using column "avatarID" as they did ----
+modelSEM1 = {
+  "emp_scal ~ b*mas_beli +
+              c*cond101 +
+              d1*age + 
+              d2*violent
+   mas_beli ~ a*cond101 + 
+              w*gend_con +
+              z*avatad_IDcent +
+              aw*gender_codxcond101 + 
+              az*avatad_IDcentxcond101 +
+              wz*avatad_IDcentxgendercod +
+              awz*avatad_IDcentxgendercodxcond101 +
+              e1*age +
+              e2*violent
+  # Indirect and total effects for males (gend_cod == 1)
+  indirectM := a*b + w*b + z*b + aw*b + az*b + wz*b + awz*b
+  totalM := indirectM + c
+  # Indirect and total effects for females (gend_cod == 0)
+  indirectF := a*b + az*b # aw*b, wz*b, and awz*b are zero
+  totalF := indirectF + c  # cw and cwx are zero"
+}
+# Is it possible to retrieve unconditional indirect and total effects?
+#ab := a*b + aw*b + ax*b + awx*b # WIP: I don't know what I'm doing
+#total := ab + c + cw + cx + cwx # WIP: I don't know what I'm doing
+
+fit1 = sem(modelSEM1, data = dat, se = "boot")
+summary(fit1)
+
+# Modeling indirect effect using full scale composite "avatar_id" ----
 # Make the model:
 modelSEM2 = {
   "emp_scal ~ b*mas_beli +
@@ -71,12 +80,7 @@ modelSEM2 = {
 fit2 = sem(modelSEM2, data = dat, se = "boot")
 summary(fit2)
 
-# Full scales for MRNI and empathy ----
-dat$emp_scal_full = dat %>% 
-  select(Empathy_comprensione:Empathy_affettuosita, 
-         Empathy_serenitaR:Empathy_disinteresseR) %>% 
-  apply(., 1, mean)
-
+# Modeling using full MRNI and full empathy scales ----
 modelSEM3 = {
   "emp_scal_full ~ b*full_MRNI +
                   c*cond101 + 
@@ -103,22 +107,24 @@ summary(fit3)
 # Full scales for MRNI, empathy, and avatar-id
 modelSEM4 = {
   "emp_scal_full ~ b*full_MRNI +
-  c*cond101 + 
-  cw*gender_codxcond101 + 
-  cx*IDcxcond101 +
-  cwx*IDcxcond101xGender
+  c*cond101 +
+  d1*violent +
+  d2*age
 
   full_MRNI ~ a*cond101 + 
   aw*gender_codxcond101 + 
-  ax*IDcxcond101 +
-  awx*IDcxcond101xGender
+  az*IDcxcond101 +
+  wz*IDcxGender +
+  awz*IDcxcond101xGender +
+  e1*violent +
+  e2*age
 
   # Indirect and total effects for males (gend_cod == 1)
-  indirectM := a*b + aw*b + ax*b + awx*b
-  totalM := indirectM + c + cw + cx + cwx
+  indirectM := a*b + aw*b + az*b + wz*b + awz*b
+  totalM := indirectM + c 
   # Indirect and total effects for females (gend_cod == 0)
-  indirectF := a*b + ax*b # aw*b and awx*b are zero
-  totalF := indirectF + c + cx # cw and cwx are zero"
+  indirectF := a*b + az*b # aw*b, wz*b, and awz*b are zero
+  totalF := indirectF + c"
 }
 
 fit4 = sem(modelSEM4, data = dat, se = "boot")
